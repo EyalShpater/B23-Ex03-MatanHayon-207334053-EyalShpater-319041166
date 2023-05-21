dc68346e04e04862e9dd494d4ad26bac6055d591
shpater1234 <shpaters@hotmail.com>
2023-05-21 15:53:37 +0300

    order

--- Ex03.GarageLogic/Order.cs
+++ Ex03.GarageLogic/Order.cs
@@ -1,9 +1,10 @@
-﻿using System.Text;
+﻿using System;
 
 namespace Ex03.GarageLogic
 {
     internal class Order
     {
+        private const int k_PhoneNumberLength = 10;
         private Vehicle m_Vehicle;
         private string m_CustomerName;
         private string m_PhoneNumber;
@@ -11,26 +12,81 @@ namespace Ex03.GarageLogic
 
         public Vehicle Vehicle
         {
-            get { return m_Vehicle; }
-            set { m_Vehicle = value; }
+            get 
+            { 
+                return m_Vehicle; 
+            }
+
+            set 
+            {
+                if (value != null)
+                {
+                    m_Vehicle = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
         }
 
         public string CustomerName
         {
-            get { return m_CustomerName; }
-            set { m_CustomerName = value; }
+            get 
+            { 
+                return m_CustomerName; 
+            }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    m_CustomerName = value;
+                }
+                else 
+                { 
+                    throw new FormatException(); 
+                }
+            }
         }
 
         public string PhoneNumber
         {
-            get { return m_PhoneNumber; }
-            set { m_PhoneNumber = value; }
+            get 
+            {
+                return m_PhoneNumber; 
+            }
+
+            set 
+            {
+                if (!string.IsNullOrEmpty(value) && int.TryParse(value, out int unusedDummyParameter))
+                {
+                    if (value.Length == k_PhoneNumberLength)
+                    {
+                        m_PhoneNumber = value;
+                    }
+                    else
+                    {
+                        throw new ValueOutOfRangeException(k_PhoneNumberLength, k_PhoneNumberLength);
+                    }
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
         }
 
         public eStatus Status
         {
-            get { return m_Status; }
-            set { m_Status = value; }
+            get 
+            { 
+                return m_Status; 
+            }
+
+            set 
+            { 
+                m_Status = value; 
+            }
         }
 
         public override string ToString()
